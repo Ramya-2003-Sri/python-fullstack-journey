20c7273b7c55dba396abb20ac0d4ca25e2bca6e0
Ramya-2003-Sri <ramyavanama660@gmail.com>
2025-08-11 16:04:21 +0530

    Create day1_intro.py

---
+++ day1_intro.py
@@ -0,0 +1,12 @@
+#day1 - python basics introduction
+#variables
+name= "ramya"
+age= 23
+is_student= True
+print(f"hello,{name}")
+print(f"Age:{age}")
+print(f"student:{is_student}")
+#checking the type of each variable
+print(type(name))
+print(type(age))
+print(type(is_student))
